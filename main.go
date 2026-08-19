a0a15f7978cebde809f65b1427088a61c99a95d8
Paulo Gustavo <gustavoks2001@gmail.com>
2026-08-19 20:43:09 -0300

    refactor: move templateHandler out of main.go into handlers.go

--- main.go
+++ main.go
@@ -4,23 +4,8 @@ import (
 	"flag"
 	"log"
 	"net/http"
-	"path/filepath"
-	"sync"
-	"text/template"
 )
 
-type templateHandler struct {
-	once     sync.Once
-	filename string
-	templ    *template.Template
-}
-
-func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	t.once.Do(func() {
-		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
-	})
-	t.templ.Execute(w, r)
-}
 func main() {
 	var addr = flag.String("addr", ":8080", "http address")
 	flag.Parse()
