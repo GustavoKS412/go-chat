6874c714d4cf1c043865f9d024aa7e4b7e6a1e92
Paulo Gustavo <gustavoks2001@gmail.com>
2026-08-18 22:06:53 -0300

    feat(client): add websocket read and write handlers

--- client.go
+++ client.go
@@ -1,4 +1,34 @@
 package main
 
+import "github.com/gorilla/websocket"
+
 type client struct {
+	socket *websocket.Conn
+
+	receive chan []byte
+
+	room *room
+}
+
+func (c *client) read() {
+
+	defer c.socket.Close()
+
+	for {
+		_, message, err := c.socket.ReadMessage()
+		if err != nil {
+			return
+		}
+		c.room.forward <- message
+	}
+}
+
+func (c *client) write() {
+	defer c.socket.Close()
+	for message := range c.receive {
+		err := c.socket.WriteMessage(websocket.TextMessage, message)
+		if err != nil {
+			return
+		}
+	}
 }
